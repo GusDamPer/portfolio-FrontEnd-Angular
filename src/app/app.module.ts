2b796f63ac3d0eaf5052030503f345f5c34cd155
GusDamPer <gusdamper@gmail.com>
2023-04-19 10:42:59 -0300

    Generando los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AcercadeComponent } from './components/acercade/acercade.component';
+import { ExpeEduComponent } from './components/expe-edu/expe-edu.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { PresentacionComponent } from './components/presentacion/presentacion.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AcercadeComponent,
+    ExpeEduComponent,
+    FooterComponent,
+    SkillsComponent,
+    PresentacionComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule
